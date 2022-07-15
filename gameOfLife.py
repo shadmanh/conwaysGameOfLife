cd946722501e11d75fde3cd3da456aaff2e8d60f
Shadman Hassan <shadman.hassan@live.ca>
2022-07-15 00:32:57 -0400

    Improved code readability

--- gameOfLife.py
+++ gameOfLife.py
@@ -2,12 +2,12 @@
 # By: Shadman Hassan
 
 # Here is my version of Conway's game of life. This was built in Python 3.9.0 with Pygame 2.1.2, SDL 2.0.18.
-# The program accepts input coordinates of the form:
+# The program accepts lines of input coordinates of the form:
 # (x, y)
 # where x, y are in the 64 bit signed range of integers. The program will start with the input at generation 0
 # and will draw the generation on screen with each alive cell being represented as a white square. Every
 # subsequent generation will be drawn until any key is pressed by the user, at which point the program will
-# terminate.
+# terminate. In the render, (0,0) will be at the center of the screen.
 
 import re
 import pygame
@@ -16,8 +16,8 @@ import pygame
 class gameOfLife:
     # Offset array used to find the neighbour coordinates of any cell
     NEIGHBOUR_ADJUSTMENT = [(-1, -1), (-1, 0), (-1, 1),
-                            (0, -1), (0, 0), (0, 1),
-                            (1, -1), (1, 0), (1, 1)]
+                            ( 0, -1), ( 0, 0), ( 0, 1),
+                            ( 1, -1), ( 1, 0), ( 1, 1)]
 
     # Size of printed grid in standard output
     SIZE_OF_DISPLAY_GRID = 20
@@ -26,6 +26,7 @@ class gameOfLife:
     SCREEN_WIDTH = 1900
     SCREEN_HEIGHT = 1000
 
+    # Colours for cell and background when drawn
     CELL_COL = (255, 255, 255)
     BACKGROUND_COL = (0, 0, 0)
 
@@ -33,7 +34,6 @@ class gameOfLife:
     CELL_PIXEL_SIZE = 10
 
     # Constructor creates empty Hashset members used to keep track of the alive cells in the current and future gens.
-    # Also parses input to populate the hashset of the current gen
     def __init__(self):
         self.curGenAliveCells = set()
         self.nextGenAliveCells = set()
@@ -42,26 +42,37 @@ class gameOfLife:
         pygame.init()
         self.surface = pygame.display.set_mode((gameOfLife.SCREEN_WIDTH, gameOfLife.SCREEN_HEIGHT))
 
+    # Add cells to the hashset of alive cells in the current gen
     def addAliveCell(self, x, y):
-        self.curGenAliveCells.add((x, y))
+        cell = (x,y)
+        if self.isWithinBounds(cell):
+            self.curGenAliveCells.add(cell)
 
     def cellIsOnScreen(self, cell):
-        return (-gameOfLife.CELL_PIXEL_SIZE <= gameOfLife.SCREEN_WIDTH // 2 +
-                cell[0] * gameOfLife.CELL_PIXEL_SIZE <= gameOfLife.SCREEN_WIDTH + gameOfLife.CELL_PIXEL_SIZE and
-                -gameOfLife.CELL_PIXEL_SIZE <= gameOfLife.SCREEN_HEIGHT // 2 +
-                cell[1] * gameOfLife.CELL_PIXEL_SIZE <= gameOfLife.SCREEN_HEIGHT + gameOfLife.CELL_PIXEL_SIZE)
+        leftBorder = -gameOfLife.CELL_PIXEL_SIZE
+        rightBorder = gameOfLife.SCREEN_WIDTH + gameOfLife.CELL_PIXEL_SIZE
+        topBorder = -gameOfLife.CELL_PIXEL_SIZE
+        bottomBorder = gameOfLife.SCREEN_HEIGHT + gameOfLife.CELL_PIXEL_SIZE
 
-    # Draw cell grid with (0, 0) located in the center
+        return (leftBorder <= self.getCellScreenCoordinateX(cell) <= rightBorder and
+                topBorder <= self.getCellScreenCoordinateY(cell) <= bottomBorder)
+
+    # Draw cell grid with (0, 0) located in the center of the screen
     def drawCells(self):
         self.surface.fill(gameOfLife.BACKGROUND_COL)
-        for aliveCell in self.curGenAliveCells:
-            if (self.cellIsOnScreen(aliveCell)):
-                pygame.draw.rect(self.surface, gameOfLife.CELL_COL,
-                                 pygame.Rect(gameOfLife.SCREEN_WIDTH // 2 + aliveCell[0] * gameOfLife.CELL_PIXEL_SIZE,
-                                             gameOfLife.SCREEN_HEIGHT // 2 + aliveCell[1] * gameOfLife.CELL_PIXEL_SIZE,
-                                             gameOfLife.CELL_PIXEL_SIZE, gameOfLife.CELL_PIXEL_SIZE))
+        for cell in self.curGenAliveCells:
+            if (self.cellIsOnScreen(cell)):
+                cellRect = pygame.Rect(self.getCellScreenCoordinateX(cell), self.getCellScreenCoordinateY(cell),
+                                             gameOfLife.CELL_PIXEL_SIZE, gameOfLife.CELL_PIXEL_SIZE)
+                pygame.draw.rect(self.surface, gameOfLife.CELL_COL, cellRect)
         pygame.display.flip()
 
+    def getCellScreenCoordinateX(self, cell):
+        return gameOfLife.SCREEN_WIDTH // 2 + cell[0] * gameOfLife.CELL_PIXEL_SIZE
+
+    def getCellScreenCoordinateY(self, cell):
+        return gameOfLife.SCREEN_HEIGHT // 2 + cell[1] * gameOfLife.CELL_PIXEL_SIZE
+        
     # Returns the nth neighbour of cell.
     # Note: n = 4 will return the cell itself as NEIGHBOUR_ADJUSTMENT[4] does no adjustments to the cell.
     def getNthNeighbour(self, cell, n):
@@ -100,26 +111,25 @@ class gameOfLife:
                         if self.isWithinBounds(neighbour) and neighbour in self.curGenAliveCells and neighbour != curCell:
                             aliveNeighbours += 1
 
-                            # If there are 2-3 alive neighbours for an alive cell, the current cell will remain alive in the next generation.
+                    # If there are 2-3 alive neighbours for an alive cell, the current cell will remain alive in the next generation.
                     # If there are exactly 3 alive neighbours for a dead cell, then it will become alive in the next generation.
                     if (curCell in self.curGenAliveCells and 2 <= aliveNeighbours <= 3) or aliveNeighbours == 3:
                         self.nextGenAliveCells.add(curCell)
 
-        # Update the current generation hashset to take on the next generation of alive cells
+        # Update the current generation hashset to take on the next generation of alive cells. Reset next gen hashset
         self.curGenAliveCells = self.nextGenAliveCells.copy()
         self.nextGenAliveCells.clear()
 
         self.curGen += 1
-        self.printGrid()
 
 # Parse through input and add each tuple of coordinates to hashset of current generation of alive cells
 def getInput(game):
     while True:
         try:
-            pos = input()
-            if pos == "":
+            position = input()
+            if position == "":
                 break
-            coordinates = re.split(', |\(|\)', pos)
+            coordinates = re.split(', |\(|\)', position)
             game.addAliveCell(int(coordinates[1]), int(coordinates[2]))
         except EOFError as e:
             break
@@ -138,6 +148,9 @@ if __name__ == '__main__':
 
         game.drawCells()
 
+        game.printGrid()
+
+        # Exit if any button is pressed while running
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 running = False
